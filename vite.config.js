062fa6029cfc800773245fb8352b18eb279aab4a
ArmandoPucheta978 <121071292+ArmandoPucheta978@users.noreply.github.com>
2024-07-27 15:05:08 -0600

    vite js ruta

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,12 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+    server: {
+        port: 10000, // Cambia el puerto aquí
+        proxy: {
+            '/api': 'http://0.0.0.0:8000' // Proxy para la API si es necesario
+        }
+    },
     plugins: [
         laravel({
             input: [
